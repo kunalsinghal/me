04a0d4d3de4a8255071fc89c48f1211d9771ff97
Kunal Singhal <kunal.singhal@grammarly.com>
2021-10-03 17:20:22 -0700

    home page ready

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import { useRouter } from 'next/router'
+import React from 'react'
 
 export default function Home() {
   return (
@@ -7,18 +9,45 @@ export default function Home() {
         <title>What's up?</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100vh",
-          width: "100vw",
-          fontSize: "2em",
-        }}
-      >
-        Nothing here, do you have the right link?
-      </div>
+      <GateKeeper />
     </div>
   );
 }
+
+const GateKeeper = () => {
+  const [answer, setAnswer] = React.useState("");
+  const router = useRouter();
+
+  React.useEffect(() => {
+    const normalized = answer.trim().toLowerCase();
+    console.log(normalized);
+    if (["proposal", "propose", "engagement", "roka"].includes(normalized)) {
+      router.push("/happy-birthday");
+    }
+  }, [answer]);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        width: "100vw",
+        fontSize: "2em",
+      }}
+    >
+      <div>How do you get in?</div>
+      <input
+        style={{
+          marginTop: "10px",
+          fontSize: "0.8em",
+        }}
+        placeholder="Hint: the first flower"
+        value={answer}
+        onChange={(e) => setAnswer(e.target.value)}
+      />
+    </div>
+  );
+};
